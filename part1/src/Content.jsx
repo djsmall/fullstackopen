191d15f0d2f6f6766bc51b775251bc323b9ca258
Dan Small <danjsmall@gmail.com>
2026-04-15 23:16:56 -0400

    Exercises 1.3 - 1.5 updated app to use single JavaScript object

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -2,9 +2,18 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.content.part1} exercise={props.content.exercises1} />
-      <Part part={props.content.part2} exercise={props.content.exercises2} />
-      <Part part={props.content.part3} exercise={props.content.exercises3} />
+      <Part
+        part={props.content.parts[0].name}
+        exercise={props.content.parts[0].exercises}
+      />
+      <Part
+        part={props.content.parts[1].name}
+        exercise={props.content.parts[1].exercises}
+      />
+      <Part
+        part={props.content.parts[2].name}
+        exercise={props.content.parts[2].exercises}
+      />
     </div>
   );
 };
--- part1/src/Total.jsx
+++ part1/src/Total.jsx
@@ -1,5 +1,9 @@
 const Total = (props) => {
-  const total = props.total.reduce((sum, current) => sum + current, 0);
+  let exercises = [];
+  for (let i = 0; i < 3; i++) {
+    exercises[i] = props.total.parts[i].exercises;
+  }
+  const total = exercises.reduce((sum, current) => sum + current, 0);
   return <p>Number of exercises: {total}</p>;
 };
 export default Total;

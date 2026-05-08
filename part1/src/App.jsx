8dfa216874075a0638cd41c69c02ce1116892ff8
Dan Small <danjsmall@gmail.com>
2026-05-08 13:41:37 -0400

    Exercises 1.6 - 1.11 unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,65 @@
-import Header from "./Header.jsx";
-import Content from "./Content.jsx";
-import Total from "./Total.jsx";
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ rating, average, positive }) => {
+  return rating.all === 0 ? (
+    <p>No feedback given</p>
+  ) : (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={rating.good} />
+        <StatisticLine text="neutral" value={rating.neutral} />
+        <StatisticLine text="bad" value={rating.bad} />
+        <StatisticLine text="all" value={rating.all} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={positive.toFixed(2) + " %"} />
+      </tbody>
+    </table>
+  );
+};
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [rating, setRating] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+  });
+  const average =
+    rating.all === 0 ? 0 : (rating.good - rating.bad) / rating.all;
+  const positive = rating.all === 0 ? 0 : (rating.good / rating.all) * 100;
+
+  const handlerOnClick = (value) => {
+    const newAll = rating.all + 1;
+    const newRating = {
+      ...rating,
+      [value]: rating[value] + 1,
+      all: newAll,
+    };
+    setRating(newRating);
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content content={course} />
-      <Total total={course} />
+      <h1>give feedback</h1>
+      <Button onClick={() => handlerOnClick("good")} text="good" />
+      <Button onClick={() => handlerOnClick("neutral")} text="neutral" />
+      <Button onClick={() => handlerOnClick("bad")} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics rating={rating} average={average} positive={positive} />
     </div>
   );
 };

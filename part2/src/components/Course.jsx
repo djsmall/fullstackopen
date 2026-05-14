3479f5e6fa2d368a8c08572bc4ee688ecc638b33
Dan Small <danjsmall@gmail.com>
2026-05-14 16:52:15 -0400

    Exercises 2.2 - 2.5 completed

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -2,11 +2,17 @@ const Course = ({ course }) => {
   return (
     <div>
       <h1>{course.name}</h1>
-      {course.parts.map((course) => (
-        <p>
-          {course.name} {course.exercises}
+      {course.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
         </p>
       ))}
+      <p>
+        <b>
+          total of {course.parts.reduce((sum, curr) => sum + curr.exercises, 0)}{" "}
+          exercises
+        </b>
+      </p>
     </div>
   );
 };
---
+++ part2/src/components/Courses.jsx
@@ -0,0 +1,12 @@
+import Course from "./Course";
+
+const Courses = ({ courses }) => {
+  return (
+    <>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
+export default Courses;
